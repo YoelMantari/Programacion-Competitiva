6c05984eb8ffb9cf9c757582e0782976047fb126
Lostwin <smile_184@hotmail.com>
2024-08-04 09:07:39 -0500

    resuelto

---
+++ InterviewBit/Largest Coprime Divisor.cpp
@@ -0,0 +1,64 @@
+#include<bits/stdc++.h>
+#define ll long long int
+
+#define all(a) a.begin(),a.end()
+#define fast ios_base::sync_with_stdio(false);cin.tie(nullptr);cout.tie(nullptr);
+#define vi vector<int>
+#define vll vector<ll>
+#define vct vector
+#define pb push_back
+#define er erase
+#define fi(i,a,n,w) for(int i =a;i<n;i+=w)
+#define fl(i,a,n,w) for(ll i=a;i<n;i+=w)
+#define fr first
+#define se second
+#define sz(a) a.size()
+#define mstr(r) for(auto i:r)cout<<i<<" ";
+#define pll pair<ll,ll>
+#define pii pair<int,int>
+
+using namespace std;
+
+
+
+int mcd(int a,int b){
+	int res;
+	while(b){
+		res=a%b;
+		a=b;
+		b=res;
+	}
+	return a;
+}
+
+void solve(){
+
+	int a,b; cin>>a>>b;
+
+	int aux;
+	while(mcd(a,b)!=1){
+		aux=mcd(a,b);
+		a/=aux;
+	}
+
+	cout<<a;
+	
+		
+
+}
+
+
+int main(){
+
+#ifndef ONLINE_JUDGE
+freopen("D:/Problem Set Competitiva/input.txt","r",stdin);
+freopen("D:/Problem Set Competitiva/output.txt","w",stdout);
+#endif
+fast
+	int t; cin>>t;
+	while(t--){
+		solve();
+	}
+
+	return 0;
+}
