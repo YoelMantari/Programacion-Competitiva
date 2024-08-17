d2650edda63ec0681e8557efb8371b87b78d8845
Lostwin <smile_184@hotmail.com>
2024-08-17 18:00:02 -0500

    resuelto

---
+++ Codeforces/A. Multiplication Table.cpp
@@ -0,0 +1,56 @@
+#include<bits/stdc++.h>
+#define ll long long int
+
+#define all(a) a.begin(),a.end()
+#define fast ios_base::sync_with_stdio(false);cin.tie(nullptr);cout.tie(nullptr);
+#define vi vector<int>
+#define vll vector<ll>
+#define vct vector
+#define pb push_back
+#define er erase
+#define fi(i,a,n,w) for(int i =a;i<n;i+=w)
+#define fl(i,a,n,w) for(ll i=a;i<n;i+=w)
+#define fr first
+#define se second
+#define sz(a) a.size()
+#define mstr(r) for(auto i:r)cout<<i<<" ";
+#define pll pair<ll,ll>
+#define pii pair<int,int>
+
+using namespace std;
+
+
+int facto(int x,int n){
+	int cont=0;
+	for(int i=1;i*i<=x;i++){
+		if(x%i==0){
+			if(i*i==x and (i<=n))cont++;
+			else if(i<=n and (x/i)<=n)cont+=2;
+		}
+	}
+	return cont;
+}
+
+
+
+void solve(){
+	int n,x; cin>>n>>x;
+	cout<<facto(x,n)<<endl;
+		
+}
+
+
+int main(){
+
+#ifndef ONLINE_JUDGE
+freopen("D:/Problem Set Competitiva/input.txt","r",stdin);
+freopen("D:/Problem Set Competitiva/output.txt","w",stdout);
+#endif
+fast
+	int t=1;
+	while(t--){
+		solve();
+	}
+
+	return 0;
+}
