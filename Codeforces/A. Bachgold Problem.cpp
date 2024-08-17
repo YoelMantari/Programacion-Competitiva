c3468a4a122effa4042da3521b2c65c3bb70e1dd
Lostwin <smile_184@hotmail.com>
2024-08-17 04:24:32 -0500

    resuelto

---
+++ Codeforces/A. Bachgold Problem.cpp
@@ -0,0 +1,54 @@
+#include<bits/stdc++.h>
+#define ll long long int
+
+#define all(a) a.begin(),a.end()
+#define fast ios_base::sync_with_stdio(false);cin.tie(nullptr);cout.tie(nullptr);
+#define vi vector<int>
+#define vll vector<ll>
+#define vct vector
+#define pb push_back
+#define er erase
+#define fi(i,a,n,w) for(int i =a;i<n;i+=w)
+#define fl(i,a,n,w) for(ll i=a;i<n;i+=w)
+#define fr first
+#define se second
+#define sz(a) a.size()
+#define mstr(r) for(auto i:r)cout<<i<<" ";
+#define pll pair<ll,ll>
+#define pii pair<int,int>
+
+using namespace std;
+
+void solve(){
+	
+	int r; cin>>r;
+	int n;
+	if(r%2==0) {
+		n=r/2;
+		cout<<n<<endl;
+	}
+	else {
+		n=(r-3)/2;
+		cout<<n+1<<endl;
+		cout<<3<<" ";
+	}
+
+	fi(i,0,n,1)cout<<2<<" ";
+
+}
+
+
+int main(){
+
+#ifndef ONLINE_JUDGE
+freopen("D:/Problem Set Competitiva/input.txt","r",stdin);
+freopen("D:/Problem Set Competitiva/output.txt","w",stdout);
+#endif
+fast
+	int t=1;
+	while(t--){
+		solve();
+	}
+
+	return 0;
+}
