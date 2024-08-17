ee0be5853b731d1782e00b0efccff9e6326a9490
Lostwin <smile_184@hotmail.com>
2024-08-17 16:24:34 -0500

    resuelto

---
+++ Codeforces/B. Maximum Multiple Sum.cpp
@@ -0,0 +1,52 @@
+#include<bits/stdc++.h>
+#define ll long long int
+
+#define all(a) a.begin(),a.end()
+#define fast ios_base::sync_with_stdio(false);cin.tie(nullptr);cout.tie(nullptr);
+#define vi vector<int>
+#define vll vector<ll>
+#define vct vector
+#define pb push_back
+#define er erase
+#define fi(i,a,n,w) for(int i =a;i<n;i+=w)
+#define fl(i,a,n,w) for(ll i=a;i<n;i+=w)
+#define fr first
+#define se second
+#define sz(a) a.size()
+#define mstr(r) for(auto i:r)cout<<i<<" ";
+#define pll pair<ll,ll>
+#define pii pair<int,int>
+
+using namespace std;
+
+void solve(){
+	int n; cin>>n;
+	int k;
+	int maxi=0,val;
+	for(int i=2;i<=n;i++){
+		k=n;
+		k/=i;	
+		int res=i*(k*(k+1)/2);
+		if(maxi<=res){
+			maxi=res;
+			val=i;
+		}
+	}
+	cout<<val<<endl;
+}
+
+
+int main(){
+
+#ifndef ONLINE_JUDGE
+freopen("D:/Problem Set Competitiva/input.txt","r",stdin);
+freopen("D:/Problem Set Competitiva/output.txt","w",stdout);
+#endif
+fast
+	int t; cin>>t;
+	while(t--){
+		solve();
+	}
+
+	return 0;
+}
