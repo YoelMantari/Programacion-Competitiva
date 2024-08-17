bbdd5134b8896e01e2c19216993dc2e12304f59b
Lostwin <smile_184@hotmail.com>
2024-08-17 15:22:14 -0500

    resuelto

---
+++ Codeforces/A. Game with Integers.cpp
@@ -0,0 +1,46 @@
+#include<bits/stdc++.h>
+#define ll long long int
+
+#define all(a) a.begin(),a.end()
+#define fast ios_base::sync_with_stdio(false);cin.tie(nullptr);cout.tie(nullptr);
+#define vi vector<int>
+#define vll vector<ll>
+#define vct vector
+#define pb push_back
+#define er erase
+#define fi(i,a,n,w) for(int i =a;i<n;i+=w)
+#define fl(i,a,n,w) for(ll i=a;i<n;i+=w)
+#define fr first
+#define se second
+#define sz(a) a.size()
+#define mstr(r) for(auto i:r)cout<<i<<" ";
+#define pll pair<ll,ll>
+#define pii pair<int,int>
+
+using namespace std;
+
+void solve(){
+	
+	int n; cin>>n;
+	if(n%3==0){cout<<"Second"<<endl;return;}	
+
+	if((n-1)%3==0 or (n+1)%3==0)cout<<"First";
+	else cout<<"Second";
+	cout<<endl;
+}
+
+
+int main(){
+
+#ifndef ONLINE_JUDGE
+freopen("D:/Problem Set Competitiva/input.txt","r",stdin);
+freopen("D:/Problem Set Competitiva/output.txt","w",stdout);
+#endif
+fast
+	int t; cin>>t;
+	while(t--){
+		solve();
+	}
+
+	return 0;
+}
