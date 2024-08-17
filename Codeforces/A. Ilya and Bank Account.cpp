9fbaa4cc153905de4f786722cd81e7bfb03427a0
Lostwin <smile_184@hotmail.com>
2024-08-17 03:42:36 -0500

    resuelto

---
+++ Codeforces/A. Ilya and Bank Account.cpp
@@ -0,0 +1,52 @@
+#include<bits/stdc++.h>
+#define ll long long int
+
+#define all(a) a.begin(),a.end()
+#define fast ios_base::sync_with_stdio(false);cin.tie(nullptr);cout.tie(nullptr);
+#define vi vector<int>
+#define vll vector<ll>
+#define vct vector
+#define pb push_back
+#define er erase
+#define fi(i,a,n,w) for(int i =a;i<n;i+=w)
+#define fl(i,a,n,w) for(ll i=a;i<n;i+=w)
+#define fr first
+#define se second
+#define sz(a) a.size()
+#define mstr(r) for(auto i:r)cout<<i<<" ";
+#define pll pair<ll,ll>
+#define pii pair<int,int>
+
+using namespace std;
+
+void solve(){
+	
+	string n; cin>>n;
+	int tam=sz(n);
+
+	if(n[0]!='-')cout<<n;
+	else{	
+	
+		if((n[tam-1])<(n[tam-2])) n.erase(tam-2,1);
+		else n.erase(tam-1,1);
+		if((tam-1)==2 and n[1]=='0')cout<<0;
+		else cout<<n;
+	}
+
+}
+
+
+int main(){
+
+#ifndef ONLINE_JUDGE
+freopen("D:/Problem Set Competitiva/input.txt","r",stdin);
+freopen("D:/Problem Set Competitiva/output.txt","w",stdout);
+#endif
+fast
+	int t=1;
+	while(t--){
+		solve();
+	}
+
+	return 0;
+}
