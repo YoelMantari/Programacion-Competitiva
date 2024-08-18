5f2acc46b1889a75683a93fac68d3c693ac8bf73
Lostwin <smile_184@hotmail.com>
2024-08-17 19:41:33 -0500

    resuelto

---
+++ Codeforces/A. Nastia and Nearly Good Numbers.cpp
@@ -0,0 +1,50 @@
+#include<bits/stdc++.h>
+#define ll long long int
+
+#define all(a) a.begin(),a.end()
+#define fast ios_base::sync_with_stdio(false);cin.tie(nullptr);cout.tie(nullptr);
+#define vi vector<int>
+#define vll vector<ll>
+#define vct vector
+#define pb push_back
+#define er erase
+#define fi(i,a,n,w) for(int i =a;i<n;i+=w)
+#define fl(i,a,n,w) for(ll i=a;i<n;i+=w)
+#define fr first
+#define se second
+#define sz(a) a.size()
+#define mstr(r) for(auto i:r)cout<<i<<" ";
+#define pll pair<ll,ll>
+#define pii pair<int,int>
+
+using namespace std;
+
+void solve(){
+	
+	ll a,b; cin>>a>>b;
+	if(b==1){cout<<"NO"<<endl;return;}
+	
+	b*=3;
+	ll x=(b-1),y=1;
+	cout<<"YES"<<endl;
+	cout<<x*a<<" "<<y*a<<" "<<a*b;
+	
+
+	cout<<endl;
+}
+
+
+int main(){
+
+#ifndef ONLINE_JUDGE
+freopen("D:/Problem Set Competitiva/input.txt","r",stdin);
+freopen("D:/Problem Set Competitiva/output.txt","w",stdout);
+#endif
+fast
+	int t; cin>>t;
+	while(t--){
+		solve();
+	}
+
+	return 0;
+}
