76f3fe02bf71b27cb3888bca2b43bd8c1bc12669
Lostwin <smile_184@hotmail.com>
2024-08-17 21:47:53 -0500

    resuelto

---
+++ LeetCode/258. Add Digits.cpp
@@ -0,0 +1,58 @@
+#include<bits/stdc++.h>
+#define ll long long int
+
+#define all(a) a.begin(),a.end()
+#define fast ios_base::sync_with_stdio(false);cin.tie(nullptr);cout.tie(nullptr);
+#define vi vector<int>
+#define vll vector<ll>
+#define vct vector
+#define pb push_back
+#define er erase
+#define fi(i,a,n,w) for(int i =a;i<n;i+=w)
+#define fl(i,a,n,w) for(ll i=a;i<n;i+=w)
+#define fr first
+#define se second
+#define sz(a) a.size()
+#define mstr(r) for(auto i:r)cout<<i<<" ";
+#define pll pair<ll,ll>
+#define pii pair<int,int>
+
+using namespace std;
+
+int sum(int n){
+	int aux=0;
+	while(1){
+		aux+=n%10;
+		n/=10;
+		if(n==0){
+			if(aux<=9)break;
+			else {n=aux;aux=0;}
+		}
+	}
+
+	return aux;
+}
+
+
+void solve(){
+	
+	int n; cin>>n;
+	cout<<sum(n);
+
+}
+
+
+int main(){
+
+#ifndef ONLINE_JUDGE
+freopen("D:/Problem Set Competitiva/input.txt","r",stdin);
+freopen("D:/Problem Set Competitiva/output.txt","w",stdout);
+#endif
+fast
+	int t=1;
+	while(t--){
+		solve();
+	}
+
+	return 0;
+}
