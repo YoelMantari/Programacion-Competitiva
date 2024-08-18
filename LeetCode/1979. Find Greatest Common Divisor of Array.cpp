3b744aa567a7fe992dabb1a82110d8c0ce558fc3
Lostwin <smile_184@hotmail.com>
2024-08-18 02:49:58 -0500

    resuelto

---
+++ LeetCode/1979. Find Greatest Common Divisor of Array.cpp
@@ -0,0 +1,56 @@
+#include<bits/stdc++.h>
+#define ll long long int
+
+#define all(a) a.begin(),a.end()
+#define fast ios_base::sync_with_stdio(false);cin.tie(nullptr);cout.tie(nullptr);
+#define vi vector<int>
+#define vll vector<ll>
+#define vct vector
+#define pb push_back
+#define er erase
+#define fi(i,a,n,w) for(int i =a;i<n;i+=w)
+#define fl(i,a,n,w) for(ll i=a;i<n;i+=w)
+#define fr first
+#define se second
+#define sz(a) a.size()
+#define mstr(r) for(auto i:r)cout<<i<<" ";
+#define pll pair<ll,ll>
+#define pii pair<int,int>
+#define ingr(r) fi(i,0,sz(r),1)cin>>r[i]; 
+
+using namespace std;
+
+int findGCD(vector<int>& nums) {
+    int mini=1e4,maxi=-1e4; 
+    int tam=sz(nums);
+    fi(i,0,tam,1){
+     	if(mini>nums[i])mini=nums[i];
+       	if(maxi<nums[i])maxi=nums[i];
+    }
+
+    return __gcd(mini,maxi);
+}
+
+void solve(){
+	
+	int n; cin>>n;
+	vi r(n); ingr(r);
+	cout<<findGCD(r)<<endl;
+
+}
+
+
+int main(){
+
+#ifndef ONLINE_JUDGE
+freopen("D:/Problem Set Competitiva/input.txt","r",stdin);
+freopen("D:/Problem Set Competitiva/output.txt","w",stdout);
+#endif
+fast
+	int t; cin>>t;
+	while(t--){
+		solve();
+	}
+
+	return 0;
+}
