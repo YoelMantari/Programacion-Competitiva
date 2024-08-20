ce323d5baad5c8ca96962c37bfc3d73f12e48486
Lostwin <smile_184@hotmail.com>
2024-08-20 05:21:43 -0500

    resuelto

---
+++ Codeforces/A. EhAb AnD gCd.cpp
@@ -0,0 +1,45 @@
+#include<bits/stdc++.h>
+#define ll long long int
+
+#define all(a) a.begin(),a.end()
+#define fast ios_base::sync_with_stdio(false);cin.tie(nullptr);cout.tie(nullptr);
+#define vi vector<int>
+#define vll vector<ll>
+#define vct vector
+#define pb push_back
+#define er erase
+#define fi(i,a,n,w) for(int i =a;i<n;i+=w)
+#define fl(i,a,n,w) for(ll i=a;i<n;i+=w)
+#define fr first
+#define se second
+#define sz(a) a.size()
+#define mstr(r) for(auto i:r)cout<<i<<" ";
+#define pll pair<ll,ll>
+#define pii pair<int,int>
+#define ingr(r) fi(i,0,sz(r),1)cin>>r[i]; 
+
+using namespace std;
+
+void solve(){
+	
+	int x; cin>>x;
+
+	cout<<1<<" "<<x-1<<endl;	
+
+}
+
+
+int main(){
+
+#ifndef ONLINE_JUDGE
+freopen("D:/Problem Set Competitiva/input.txt","r",stdin);
+freopen("D:/Problem Set Competitiva/output.txt","w",stdout);
+#endif
+fast
+	int t; cin>>t;
+	while(t--){
+		solve();
+	}
+
+	return 0;
+}
