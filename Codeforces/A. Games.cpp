6b6d1a8f736b6f87739c3af02873067ca3aed02c
Lostwin <smile_184@hotmail.com>
2024-12-30 19:48:29 -0500

    Accpeted

---
+++ Codeforces/A. Games.cpp
@@ -0,0 +1,59 @@
+//#include<bits/stdc++.h>
+#include<iostream>
+#include <vector>
+#define ll long long int
+
+#define all(a) a.begin(),a.end()
+#define fast ios_base::sync_with_stdio(false);cin.tie(nullptr);cout.tie(nullptr);
+#define vi vector<int>
+#define vll vector<ll>
+#define vct vector
+#define pb push_back
+#define er erase
+#define fi(i,a,n,w) for(int i =a;i<n;i+=w)
+#define fl(i,a,n,w) for(ll i=a;i<n;i+=w)
+#define fr first
+#define se second
+#define sz(a) a.size()
+#define mstr(r) for(auto i:r)cout<<i<<" ";
+#define pll pair<ll,ll>
+#define pii pair<int,int>
+#define ingr(r) fi(i,0,sz(r),1)cin>>r[i]; 
+
+using namespace std;
+
+void solve(){
+	int t; cin>>t;
+	vector<int> r1(101,0),r2(101,0);
+	int a,b;
+	fi(i,0,t,1){
+		cin>>a>>b;
+		r1[a]++;
+		r2[b]++;
+	}
+
+	int sum = 0;
+	for(int i = 1; i<=100;i++){
+		sum += r1[i]*r2[i];
+	}
+
+	cout<<sum<<endl;
+
+
+}
+
+
+int main(){
+
+#ifndef ONLINE_JUDGE
+freopen("D:/Proyectos/C++/Problem Set Competitiva/input.txt","r",stdin);
+freopen("D:/Proyectos/C++/Problem Set Competitiva/output.txt","w",stdout);
+#endif
+fast
+	int t=1;
+	while(t--){
+		solve();
+	}
+
+	return 0;
+}
