6fdcf70ce67079a4c742e62fe7eb4cc0be8fc1f5
Lostwin <smile_184@hotmail.com>
2025-01-03 19:39:49 -0500

    Accepted

---
+++ Codeforces/A. YES or YES.cpp
@@ -0,0 +1,53 @@
+//#include<bits/stdc++.h>
+#include <cctype>
+#include<iostream>
+#define ll long long int
+
+#define all(a) a.begin(),a.end()
+#define fast ios_base::sync_with_stdio(false);cin.tie(nullptr);cout.tie(nullptr);
+#define vi vector<int>
+#define vll vector<ll>
+#define vct vector
+#define pb push_back
+#define er erase
+#define fi(i,a,n,w) for(int i =a;i<n;i+=w)
+#define fl(i,a,n,w) for(ll i=a;i<n;i+=w)
+#define fr first
+#define se second
+#define sz(a) a.size()
+#define mstr(r) for(auto i:r)cout<<i<<" ";
+#define pll pair<ll,ll>
+#define pii pair<int,int>
+#define ingr(r) fi(i,0,sz(r),1)cin>>r[i]; 
+
+using namespace std;
+
+void solve(){
+	
+	string q; cin>>q;
+
+	q[0]=toupper(q[0]);
+	q[1]=toupper(q[1]);
+	q[2]=toupper(q[2]);
+
+	if(q=="YES")cout<<"YES";
+	else cout<<"NO";
+	cout<<endl;
+
+}
+
+
+int main(){
+
+#ifndef ONLINE_JUDGE
+freopen("D:/Proyectos/C++/Problem Set Competitiva/input.txt","r",stdin);
+freopen("D:/Proyectos/C++/Problem Set Competitiva/output.txt","w",stdout);
+#endif
+fast
+	int t; cin>>t;
+	while(t--){
+		solve();
+	}
+
+	return 0;
+}
